23b022a5fdaef9ff5802cb2dbbb42a722fe1d834
Gebruiker <gebruiker@wcw-stud-145-109-20-251.wireless.uva.nl>
2018-06-06 12:10:44 +0200

    Initial Commit

---
+++ roomiesappTests/roomiesappTests.swift
@@ -0,0 +1,36 @@
+//
+//  roomiesappTests.swift
+//  roomiesappTests
+//
+//  Created by Gebruiker on 06-06-18.
+//  Copyright © 2018 Gebruiker. All rights reserved.
+//
+
+import XCTest
+@testable import roomiesapp
+
+class roomiesappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
